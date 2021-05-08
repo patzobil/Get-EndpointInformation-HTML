88c90418f0cc7c3ecc0b55b0bb551e38f945442c
patzobil <patzobil@gmail.com>
2021-05-08 17:03:34 +0200

    Write-Progress Help & Cleanup

--- functions/Write-Progress.ps1
+++ functions/Write-Progress.ps1
@@ -1,13 +1,21 @@
-#region Write-ProgressFunction
-#* ************************************** Write-Progress Function ***************************************
+<#
+.SYNOPSIS
+    A function to display a progress bar for every step of the way
+.DESCRIPTION
+    Write-ProgressHelper : A function to display a progress bar for every step of the way
+	Every time it is invoked it counts towards the total times the Write-ProgressHelper Function runs.
+	That way Write-Progress -PercentComplete will show how far the script is in the progress bar.
+.EXAMPLE
+    Write-Progress -Activity "Progress Bar Title" -Status "Current Action"
+    Basic Progress Bar Title and Action
+.EXAMPLE
+    Write-ProgressHelper "Current Action" -StepNumber ($stepCounter++)
+    Pre-defined Progress bar Title, Current Action and Step Counter
+#>
 function Write-ProgressHelper {
 	param (
         [int]$StepNumber,
         [string]$Message
 	)
-
 	Write-Progress -Activity "Collecting Software and Hardware Info for $env:computername" -Status $Message -PercentComplete (($StepNumber / $steps) * 100) 
 }
-
-#! ************************************** Write-Progress Function ***************************************
-#endregion
