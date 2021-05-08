df63495b95f86d62d31218e2de346845ea9b2b63
patzobil <patzobil@gmail.com>
2021-05-08 02:54:44 +0200

    HTMLHead cleanup

--- html/HTMLHead.ps1
+++ html/HTMLHead.ps1
@@ -4,9 +4,6 @@ $header = @"
 <title>$env:computername Information Report</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="$cssBaseLocation">
-<style>
-
-</style>
 "@
 #! ************************************** HEAD / CSS ***************************************
 #endregion
