59fd13a174bf8ce8beea56e40542431a887e2590
patzobil <patzobil@gmail.com>
2021-05-16 10:39:53 +0200

    SideNavigation JS | findActive() & activeLink()

--- html/SideNavigationBar.ps1
+++ html/SideNavigationBar.ps1
@@ -2,16 +2,16 @@
 #* ************************************** SIDE NAVIGATION BAR ***************************************
 $sideNavigation = @"
 <div class="sidenav">
-    <a class="inactive" onclick="activeLink('bioslink')" id="bioslink" href="#biosInfo">BIOS</a>
-    <a class="inactive" onclick="activeLink('cpulink')" id="cpulink" href="#CPUInfo">Processor</a>
-    <a class="inactive" onclick="activeLink('ramlink')" id="ramlink" href="#RAMInfo">RAM</a>
-    <a class="inactive" onclick="activeLink('oslink')" id="oslink" href="#OSInfo">Operating System</a>
-    <a class="inactive" onclick="activeLink('pslink')" id="pslink" href="#PSInfo">PowerShell</a>
-    <a class="inactive" onclick="activeLink('driveslink')" id="driveslink" href="#diskInfo">Drives</a>
-    <a class="inactive" onclick="activeLink('netlink')" id="netlink" href="#NICInfo">Network</a>
-    <a class="inactive" onclick="activeLink('hostslink')" id="hostslink" href="#hostsInfo">Hosts File</a>
-    <a class="inactive" onclick="activeLink('serviceslink')" id="serviceslink" href="#servicesInfo">Services</a>
-    <a class="inactive" onclick="activeLink('appslink')" id="appslink" href="#appsInfo">Applications</a>
+    <a class="disabled" onclick="activeLink('bioslink')" id="bioslink" href="#biosInfo">BIOS</a>
+    <a class="disabled" onclick="activeLink('cpulink')" id="cpulink" href="#CPUInfo">Processor</a>
+    <a class="disabled" onclick="activeLink('ramlink')" id="ramlink" href="#RAMInfo">RAM</a>
+    <a class="disabled" onclick="activeLink('oslink')" id="oslink" href="#OSInfo">Operating System</a>
+    <a class="disabled" onclick="activeLink('pslink')" id="pslink" href="#PSInfo">PowerShell</a>
+    <a class="disabled" onclick="activeLink('driveslink')" id="driveslink" href="#diskInfo">Drives</a>
+    <a class="disabled" onclick="activeLink('netlink')" id="netlink" href="#NICInfo">Network</a>
+    <a class="disabled" onclick="activeLink('hostslink')" id="hostslink" href="#hostsInfo">Hosts File</a>
+    <a class="disabled" onclick="activeLink('serviceslink')" id="serviceslink" href="#servicesInfo">Services</a>
+    <a class="disabled" onclick="activeLink('appslink')" id="appslink" href="#appsInfo">Applications</a>
 </div>
 <script type="text/javascript" src="$jsBaseFolder\SideNavigation.js"></script>
 <div class='main'>
--- js/SideNavigation.js
+++ js/SideNavigation.js
@@ -1,11 +1,14 @@
-function activeLink(id) {
+function findActive(){
     var linksArray = ['bioslink', 'cpulink', 'ramlink','oslink', 'pslink', 'driveslink','netlink', 'hostslink', 'serviceslink', 'appslink'];
     linksArray.forEach(
         function (elemIds) {
         document.getElementById(elemIds).setAttribute("class", "inactive");
         }       
-    );
+    ); 
+}
 
+function activeLink(id) {
+    findActive();
     var elem = document.getElementById(id);
     elem.setAttribute("class", "active");
 }
