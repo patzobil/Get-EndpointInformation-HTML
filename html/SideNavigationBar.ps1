f22593984df039dea9cdcf47d5a110ae1e7de52f
patzobil <patzobil@gmail.com>
2021-05-17 01:59:40 +0200

    CSS - JS accordion sidenav links

--- html/SideNavigationBar.ps1
+++ html/SideNavigationBar.ps1
@@ -2,19 +2,49 @@
 #* ************************************** SIDE NAVIGATION BAR ***************************************
 $sideNavigation = @"
 <div class="sidenav">
-    <a class="disabled" onclick="activeLink('bioslink')" id="bioslink" href="#biosInfo">BIOS</a>
-    <a class="disabled" onclick="activeLink('cpulink')" id="cpulink" href="#CPUInfo">Processor</a>
-    <a class="disabled" onclick="activeLink('ramlink')" id="ramlink" href="#RAMInfo">RAM</a>
-    <a class="disabled" onclick="activeLink('oslink')" id="oslink" href="#OSInfo">Operating System</a>
-    <a class="disabled" onclick="activeLink('pslink')" id="pslink" href="#PSInfo">PowerShell</a>
-    <a class="disabled" onclick="activeLink('driveslink')" id="driveslink" href="#diskInfo">Drives</a>
-    <a class="disabled" onclick="activeLink('netlink')" id="netlink" href="#NICInfo">Network</a>
-    <a class="disabled" onclick="activeLink('hostslink')" id="hostslink" href="#hostsInfo">Hosts File</a>
-    <a class="disabled" onclick="activeLink('serviceslink')" id="serviceslink" href="#servicesInfo">Services</a>
-    <a class="disabled" onclick="activeLink('appslink')" id="appslink" href="#appsInfo">Applications</a>
+    <a class="accordion" id="oslink" href="#OSInfo">Operating System</a>
+    <a class="accordion" id="pslink" href="#PSInfo">PowerShell</a>
+    <a class="accordion" id="hardwarelink" href="#biosInfo">Hardware</a>
+    <div class="panel">
+    <a class="" id="bioslink" href="#biosInfo">BIOS</a>
+    <a class="" id="cpulink" href="#CPUInfo">Processor</a>
+    <a class="" id="ramlink" href="#RAMInfo">RAM</a>
+    </div>
+    <a class="accordion" id="driveslink" href="#diskInfo">Drives</a>
+    <a class="accordion" id="netlink" href="#NICInfo">Network</a>
+    <div class="panel">
+    <a class="" id="hostslink" href="#hostsInfo">Hosts File</a>
+    </div>
+    <a class="accordion" id="serviceslink" href="#servicesInfo">Services</a>
+    <a class="accordion" id="appslink" href="#appsInfo">Applications</a>
 </div>
+
 <script type="text/javascript" src="$jsBaseFolder\SideNavigation.js"></script>
 <div class='main'>
 "@
 #! ************************************** SIDE NAVIGATION BAR ***************************************
 #endregion
+
+
+<# 
+
+<div class="sidenav">
+    <a class="accordion" onclick="activeLink('oslink')" id="oslink" href="#OSInfo">Operating System</a>
+    <a class="accordion" onclick="activeLink('pslink')" id="pslink" href="#PSInfo">PowerShell</a>
+    <a class="accordion" onclick="activeLink('hardwarelink')" id="hardwarelink" href="#biosInfo">Hardware</a>
+    <div class="panel">
+    <a class="accordion" onclick="activeLink('bioslink')" id="bioslink" href="#biosInfo">BIOS</a>
+    <a class="accordion" onclick="activeLink('cpulink')" id="cpulink" href="#CPUInfo">Processor</a>
+    <a class="accordion" onclick="activeLink('ramlink')" id="ramlink" href="#RAMInfo">RAM</a>
+    </div>
+    <a class="accordion" onclick="activeLink('driveslink')" id="driveslink" href="#diskInfo">Drives</a>
+    <a class="accordion" onclick="activeLink('netlink')" id="netlink" href="#NICInfo">Network</a>
+    <div class="panel">
+    <a class="accordion" onclick="activeLink('hostslink')" id="hostslink" href="#hostsInfo">Hosts File</a>
+    </div>
+    <a class="accordion" onclick="activeLink('serviceslink')" id="serviceslink" href="#servicesInfo">Services</a>
+    <a class="accordion" onclick="activeLink('appslink')" id="appslink" href="#appsInfo">Applications</a>
+</div>
+
+
+#>
--- js/SideNavigation.js
+++ js/SideNavigation.js
@@ -1,8 +1,8 @@
 function findActive(){
-    var linksArray = ['bioslink', 'cpulink', 'ramlink','oslink', 'pslink', 'driveslink','netlink', 'hostslink', 'serviceslink', 'appslink'];
+    var linksArray = ['hardwarelink', 'bioslink', 'cpulink', 'ramlink','oslink', 'pslink', 'driveslink','netlink', 'hostslink', 'serviceslink', 'appslink'];
     linksArray.forEach(
         function (elemIds) {
-        document.getElementById(elemIds).setAttribute("class", "inactive");
+        document.getElementById(elemIds).setAttribute("class", "accordion");
         }       
     ); 
 }
@@ -10,5 +10,33 @@ function findActive(){
 function activeLink(id) {
     findActive();
     var elem = document.getElementById(id);
-    elem.setAttribute("class", "active");
+    // elem.setAttribute("class", "active");
+}
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+
+
+//Panel-like Items
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        findActive();
+        // this.setAttribute("class", "active");
+        this.classList.toggle("active");
+        // this.classList.toggle("inactive");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
